3caea3436f0ec391e6977f4f4d9bc03032723cd7
Mitko Viki <mitkoviki@ubuntu.(none)>
2013-09-29 23:02:58 +0300

    ASCIITableProg

--- Homework_1.c
+++ Homework_1.c
@@ -2,8 +2,8 @@
 int main () {
 	char charsim=0;
 	int broqch=0;
-	while (broqch<255) {
-		printf("%c-%d \n",charsim,broqch);
+	while (broqch<256) {
+		printf("%d-%c \n",broqch,charsim);
 		broqch++;
 		charsim++;
 	}
